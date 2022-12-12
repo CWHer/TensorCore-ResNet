12c4da272d17ee0d7b0f01f493261767bdba3139
CWHer <cwher@outlook.com>
2022-12-12 07:30:14 +0000

    test: update device test

--- resnet/tests/test_tensor.cpp
+++ resnet/tests/test_tensor.cpp
@@ -16,6 +16,9 @@ TEST(tensor, basic)
     EXPECT_EQ(x.index({1, 6}), value);
     x.view({24});
     EXPECT_EQ(x.index({18}), value);
+
+    Tensor y;
+    EXPECT_EQ(y.empty(), true);
 }
 
 TEST(tensor, load)
@@ -35,4 +38,26 @@ TEST(tensor, load)
     EXPECT_NEAR(x.index({0, 0, 0, 0}), value1, eps);
     EXPECT_NEAR(x.index({0, 0, 0, 1}), value2, eps);
     EXPECT_NEAR(x.index({0, 0, 0, 2}), value3, eps);
+}
+
+TEST(tensor, device)
+{
+    // NOTE: pretrained resnet18 model
+    std::string filename = "conv1_weight.bin";
+    Tensor x;
+    x.load(filename);
+    EXPECT_EQ(x.getDevice(), DeviceType::CPU);
+
+    x.to(DeviceType::CUDA);
+    EXPECT_EQ(x.getDevice(), DeviceType::CUDA);
+
+    Tensor y = x.clone();
+    EXPECT_EQ(y.getDevice(), DeviceType::CUDA);
+    const float value1 = -1.0419e-02;
+    const float value2 = -6.1356e-03;
+    const float value3 = -1.8098e-03;
+    const float eps = 1e-3;
+    EXPECT_NEAR(x.index({0, 0, 0, 0}), value1, eps);
+    EXPECT_NEAR(y.index({0, 0, 0, 1}), value2, eps);
+    EXPECT_NEAR(y.index({0, 0, 0, 2}), value3, eps);
 }
