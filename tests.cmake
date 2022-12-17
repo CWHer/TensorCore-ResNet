042205ca259db3f50b1d1eb42a330bff25d59ef1
CWHer <cwher@outlook.com>
2022-12-17 18:21:06 +0000

    test: add test_half

--- tests.cmake
+++ tests.cmake
@@ -1,4 +1,5 @@
 # Add all unit tests here
 set(TEST_SOURCE
     tests/test_device_memory.cpp
+    tests/test_half.cpp
 )
---
+++ tests/test_half.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include "half.hpp"
+
+TEST(half, conversion)
+{
+    Sim::Half::FP32 x;
+    Sim::Half h;
+
+    const float eps = 1e-4f;
+
+    // normalized value
+    x.f = 10.0f;
+    h.fromFloat(x.f);
+    EXPECT_NEAR(x.f, h.toFloat(), x.f * eps);
+
+    // normalized value: overflow
+    x.f = std::numeric_limits<float>::max();
+    h.fromFloat(x.f);
+    EXPECT_TRUE(std::isinf(h.toFloat()));
+
+    // normalized value: underflow
+    x.f = std::numeric_limits<float>::min();
+    h.fromFloat(x.f);
+    EXPECT_EQ(0, h.toFloat());
+
+    // normalized value: underflow
+    x.f = 1.0f / (1 << 20);
+    h.fromFloat(x.f);
+    EXPECT_NEAR(x.f, h.toFloat(), x.f * eps);
+
+    // denormalized value
+    x.f = std::numeric_limits<float>::denorm_min();
+    h.fromFloat(x.f);
+    EXPECT_EQ(0, h.toFloat());
+
+    // infinity
+    x.f = std::numeric_limits<float>::infinity();
+    h.fromFloat(x.f);
+    EXPECT_TRUE(std::isinf(h.toFloat()));
+
+    // NaN
+    x.f = std::numeric_limits<float>::quiet_NaN();
+    h.fromFloat(x.f);
+    EXPECT_TRUE(std::isnan(h.toFloat()));
+}
