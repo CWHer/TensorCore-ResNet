5835f0e34997a697a568cd7ea53a93179beeb91b
CWHer <cwher@outlook.com>
2022-12-18 12:15:15 +0000

    to: add incomplete gemm functions

--- src/functions.hpp
+++ src/functions.hpp
@@ -1,10 +1,78 @@
 #pragma once
 
 #include "common.h"
+#include "simulator.hpp"
 
 namespace Sim
 {
-    // TODO: migrate original code here
+
+    void wmma_kernel(f16 *a, f16 *b, f32 *c){
+        // NOTE: wmma functions
+        // wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::row_major> a_frag;
+        // wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::col_major> b_frag;
+        // wmma::fragment<wmma::accumulator, 16, 16, 16, float> c_frag;
+
+        // wmma::fill_fragment(c_frag, 0.0f);
+        // wmma::load_matrix_sync(a_frag, a, 16);
+        // wmma::load_matrix_sync(b_frag, b, 16);
+        // wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+        // wmma::store_matrix_sync(c, c_frag, 16, wmma::mem_row_major);
+
+        // TODO:
+    };
+
+    void device_gemm(Sim::GPUSimulator &sim, const Sim::dim3 &block_dim,
+                     const std::array<Sim::unit3, Sim::GPUSimulator::WARP_SIZE> &warp,
+                     const f32 *a, const f32 *b, f32 *c, int m, int n, int k)
+    {
+        for (const auto &thread_idx : warp)
+        {
+            static const int N_WMMA = 16;
+
+            f16 a_frag[N_WMMA][N_WMMA], b_frag[N_WMMA][N_WMMA];
+            f32 c_frag[N_WMMA][N_WMMA];
+
+            int row = thread_idx.y * N_WMMA;
+            int col = thread_idx.x * N_WMMA;
+            for (int i = 0; i < N_WMMA; i++)
+                for (int j = 0; j < N_WMMA; j++)
+                    if (row + i < m && col + j < n)
+                    {
+                        // TODO: check this
+                        a_frag[i][j].fromFloat(a[(row + i) * k + col + j]);
+                        b_frag[i][j].fromFloat(b[(row + i) * k + col + j]);
+                        c_frag[i][j] = c[(row + i) * k + col + j];
+                    }
+            wmma_kernel((f16 *)a_frag, (f16 *)b_frag, (f32 *)c_frag);
+
+            for (int i = 0; i < N_WMMA; i++)
+                for (int j = 0; j < N_WMMA; j++)
+                    if (row + i < m && col + j < n)
+                        c[(row + i) * k + col + j] = c_frag[i][j];
+        }
+    }
+
+    void host_gemm(const f32 *a, const f32 *b, f32 *c,
+                   int m, int n, int k, Sim::GPUSimulator &sim)
+    {
+        f32 *d_a, *d_b, *d_c;
+        sim.cudaMalloc((void **)&d_a, m * k * sizeof(f32));
+        sim.cudaMalloc((void **)&d_b, k * n * sizeof(f32));
+        sim.cudaMalloc((void **)&d_c, m * n * sizeof(f32));
+
+        sim.cudaMemcpy(d_a, (void *)a, m * k * sizeof(f32), CUDAMemcpyType::MemcpyDeviceToHost);
+        sim.cudaMemcpy(d_b, (void *)b, k * n * sizeof(f32), CUDAMemcpyType::MemcpyDeviceToHost);
+
+        // NOTE: block_dim.z is just a reminder with no use
+        static const int N_WMMA = 16;
+        // TODO: padding
+        dim3 block_dim(16, 16, 32);
+        sim.launchKernel(block_dim, device_gemm, d_a, d_b, d_c, m, n, k);
+
+        sim.cudaMemcpy((void *)c, d_c, m * n * sizeof(f32), CUDAMemcpyType::MemcpyDeviceToHost);
+        sim.cudaFree(d_a);
+        sim.cudaFree(d_b);
+    }
 
     // void wmma_kernel(__half *a, __half *b, float *c, float *d, dim3 &gridDim,
     //                  dim3 &blockDim, GPU &volta)
