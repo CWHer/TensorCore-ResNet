b565365369c452f6161d618777e2dc1002957821
CWHer <cwher@outlook.com>
2022-12-18 05:01:33 +0000

    feat: add reg_file

--- src/common.h
+++ src/common.h
@@ -16,6 +16,7 @@
 #include <thread>
 #include <mutex>
 #include <bitset>
+#include <array>
 
 namespace Sim
 {
--- src/reg_file.hpp
+++ src/reg_file.hpp
@@ -5,12 +5,42 @@
 namespace Sim
 {
 
-    class RegisterFile
+    template <typename T, u32 MAX_REG_NUM_PER>
+    class RegisterFileBase
     {
     private:
-        // NOTE: R255 is RZ, which is always 0
-        static const u32 MAX_REG_NUM_PER = 256;
-        // std::unordered_map<u32, std::vector<>> reg_file;
+        static const std::unordered_map<u32, T> SPECIAL_REG;
+        std::unordered_map<u32, std::array<T, MAX_REG_NUM_PER>> reg_file;
+
+    public:
+        RegisterFileBase() { reset(); }
+
+        void reset() { reg_file.clear(); }
+
+        void write(u32 thread_idx, u32 reg_idx, T value)
+        {
+            if (SPECIAL_REG.count(reg_idx) == 0)
+            {
+                reg_file[thread_idx][reg_idx] = value;
+            }
+        }
+
+        T read(u32 thread_idx, u32 reg_idx)
+        {
+            return SPECIAL_REG.count(reg_idx) == 0
+                       ? reg_file[thread_idx][reg_idx]
+                       : SPECIAL_REG.at(reg_idx);
+        }
     };
+
+    // clang-format off
+    // NOTE: R7 is PT, which is always true
+    using PRegisterFile = RegisterFileBase<bool, 8>;
+    template <> const std::unordered_map<u32, bool> PRegisterFile::SPECIAL_REG = {{7, true}};
+
+    // NOTE: R255 is RZ, which is always 0
+    using RegisterFile = RegisterFileBase<u32, 256>;
+    template <> const std::unordered_map<u32, u32> RegisterFile::SPECIAL_REG = {{255, 0}};
+    // clang-format on
 
 }
