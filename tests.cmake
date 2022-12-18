dd613e1cfee3b010f851df48cf88e65c53b683c3
CWHer <cwher@outlook.com>
2022-12-18 05:01:45 +0000

    test: add reg_file test

--- tests.cmake
+++ tests.cmake
@@ -2,4 +2,5 @@
 set(TEST_SOURCE
     tests/test_device_memory.cpp
     tests/test_half.cpp
+    tests/test_regfile.cpp
 )
---
+++ tests/test_regfile.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+#include "reg_file.hpp"
+
+TEST(reg, reg_file)
+{
+    Sim::RegisterFile reg_file;
+
+    unsigned thread_idx0 = 1 << 30;
+    unsigned thread_idx1 = 1 << 20;
+    unsigned value0 = 1234;
+    unsigned value1 = 5678;
+
+    reg_file.write(thread_idx0, 0, value0);
+    reg_file.write(thread_idx1, 0, value1);
+    EXPECT_EQ(reg_file.read(thread_idx0, 0), value0);
+    EXPECT_EQ(reg_file.read(thread_idx1, 0), value1);
+
+    reg_file.write(thread_idx0, 255, value0);
+    EXPECT_EQ(reg_file.read(thread_idx0, 255), 0);
+
+    reg_file.reset();
+    EXPECT_EQ(reg_file.read(thread_idx0, 0), 0);
+}
+
+TEST(reg, preg_file)
+{
+    Sim::PRegisterFile preg_file;
+
+    unsigned thread_idx0 = 1 << 30;
+    unsigned thread_idx1 = 1 << 20;
+    bool value0 = true;
+    bool value1 = false;
+
+    preg_file.write(thread_idx0, 0, value0);
+    preg_file.write(thread_idx1, 0, value1);
+    EXPECT_EQ(preg_file.read(thread_idx0, 0), value0);
+    EXPECT_EQ(preg_file.read(thread_idx1, 0), value1);
+
+    preg_file.write(thread_idx0, 7, value0);
+    EXPECT_EQ(preg_file.read(thread_idx0, 7), true);
+
+    preg_file.reset();
+    EXPECT_EQ(preg_file.read(thread_idx0, 0), false);
+}
