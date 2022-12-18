f2e000722f95f70166e1f8479be59b861a5f3e94
CWHer <cwher@outlook.com>
2022-12-18 06:48:26 +0000

    feat: add launch kernel & corresponding test

--- tests/test_simulator.cpp
+++ tests/test_simulator.cpp
@@ -51,6 +51,39 @@ TEST(gpu_simulator, memory)
     delete[] h_ptr;
 }
 
+TEST(gpu_simulator, launch_kernel)
+{
+    Sim::GlobalMemory mem;
+    Sim::RegisterFile reg_file;
+    Sim::PRegisterFile preg_file;
+
+    Sim::GPUSimulator sim(mem, reg_file, preg_file);
+
+    auto dummy_kernel = [](Sim::GPUSimulator &sim, const Sim::dim3 &block_dim,
+                           const std::array<Sim::unit3, Sim::GPUSimulator::WARP_SIZE> &warp,
+                           float *matrix_ptr)
+    {
+        for (const auto &thread_idx : warp)
+        {
+            int idx = thread_idx.x + thread_idx.y * block_dim.x;
+            matrix_ptr[idx] = 1.0f;
+        }
+    };
+
+    float h_matrix[16][16] = {0};
+    float *d_matrix = nullptr;
+    sim.cudaMalloc((void **)&d_matrix, 16 * 16 * sizeof(float));
+    Sim::dim3 block_dim(16, 16);
+    sim.launchKernel(block_dim, dummy_kernel, d_matrix);
+    sim.cudaMemcpy(h_matrix, d_matrix, 16 * 16 * sizeof(float),
+                   Sim::CUDAMemcpyType::MemcpyDeviceToHost);
+
+    const float eps = 1e-6f;
+    for (int i = 0; i < 16; i++)
+        for (int j = 0; j < 16; j++)
+            EXPECT_NEAR(h_matrix[i][j], 1.0f, eps);
+}
+
 TEST(gpu_simulator, instructions)
 {
     // TODO
