3acbe5d511a1bcbb65ea5c28692f764a63336907
CWHer <cwher@outlook.com>
2022-12-23 12:37:34 +0000

    fix: fix BN interface and storage

--- resnet/modules/batchnorm.hpp
+++ resnet/modules/batchnorm.hpp
@@ -8,29 +8,29 @@
 class BatchNorm2d : public Module
 {
 private:
-    float eps;
     int num_features;
-    float *mean_data, *var_data, *weight_data, *bias_data;
+    double eps;
+    Tensor weight, bias, running_mean, running_var;
 
 public:
-    BatchNorm2d(Tensor weight, Tensor bias, Tensor running_mean, Tensor running_var, float eps = 1e-5) : eps(eps)
+    // FIXME: these parameters are not used,
+    //  recommend to add conflict check or remove them
+    BatchNorm2d(int num_features, double eps = 1e-5, double momentum = 0.1,
+                bool affine = true, bool track_running_stats = true)
+        : num_features(num_features), eps(eps)
     {
         addTensor("weight", weight);             // [num_features]
         addTensor("bias", bias);                 // [num_features]
         addTensor("running_mean", running_mean); // [num_features]
         addTensor("running_var", running_var);   // [num_features]
-        weight_data = weight.data_ptr();
-        bias_data = bias.data_ptr();
-        mean_data = running_mean.data_ptr();
-        var_data = running_var.data_ptr();
-        num_features = weight.sizes()[0];
     }
 
     Tensor forward(Tensor x) override
     {
         checkCppErrorsMsg(x.sizes().size() != 4, "BatchNorm2d only support 4D input");
         checkCppErrorsMsg(x.sizes()[1] != num_features, "BatchNorm2d input channel size mismatch");
-        checkCppErrorsMsg(x.getDevice() != DeviceType::CPU,
+        checkCppErrorsMsg(x.getDevice() != DeviceType::CUDA ||
+                              weight.getDevice() != DeviceType::CUDA,
                           "BatchNorm2d only support CUDA");
 
         // NOTE: x is in NCHW format
@@ -41,10 +41,15 @@ public:
         unsigned int width = x.sizes()[3];
 
         float *input_data = x.data_ptr();
-        
-        BatchNorm2dKernel(input_data, mean_data, var_data, weight_data, bias_data, eps,
-                          batch_size, num_channels, height, width);
-        
+        float *mean_data = running_mean.data_ptr();
+        float *var_data = running_var.data_ptr();
+        float *weight_data = weight.data_ptr();
+        float *bias_data = bias.data_ptr();
+
+        hostBatchNorm2d(input_data, mean_data, var_data, weight_data, bias_data,
+                        eps, batch_size, num_channels, height, width);
+
+        // HACK: FIXME: this module is currently inplace
         return x;
     }
 
