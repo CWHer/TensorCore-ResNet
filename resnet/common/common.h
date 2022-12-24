ccb722b41ea49463a042d63d076ca9425127f0e3
#ifndef <58112438+Vieran@users.noreply.github.com>
2022-12-24 14:39:29 +0800

    merge: merge bn-dev
    * feat: bn modules & test & py test utility
    
    * feat: add random generator in common
    
    * fix: fix default constructor & load of tensor
    
    * fix: add suffix in load weights
    
    * chore: upd sources.cmake & tests.cmake

--- resnet/common/common.h
+++ resnet/common/common.h
@@ -13,6 +13,7 @@
 #include <memory>
 #include <cmath>
 #include <numeric>
+#include <random>
 
 // utility functions
 enum DeviceType
@@ -93,3 +94,19 @@ public:
         return duration.count();
     }
 };
+
+int randomInt(int min, int max)
+{
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(min, max);
+    return dist(gen);
+}
+
+float randomFloat(float min, float max)
+{
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dist(min, max);
+    return dist(gen);
+}
