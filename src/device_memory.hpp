35d0dc1deca4b2d7069af4fd538897dd02a84c73
CWHer <cwher@outlook.com>
2022-12-26 18:15:04 +0000

    feat: add bytes read & write

--- src/device_memory.hpp
+++ src/device_memory.hpp
@@ -12,30 +12,68 @@ namespace Sim
         friend class GPUSimulator;
 
     private:
-        std::unordered_set<intptr_t> allocated_addrs;
+        // (address, # bytes)
+        std::unordered_map<intptr_t, u64> allocated_addrs;
 
     public:
         GlobalMemory() = default;
 
+        bool isAllocated(void *ptr)
+        {
+            auto ptr_addr = reinterpret_cast<intptr_t>(ptr);
+            return allocated_addrs.count(ptr_addr) > 0;
+        }
+
         void cudaMalloc(void **ptr, size_t size)
         {
             *ptr = new char[size];
-            allocated_addrs.insert(reinterpret_cast<intptr_t>(*ptr));
+            allocated_addrs[reinterpret_cast<intptr_t>(*ptr)] = size;
         }
 
-        template <typename T>
-        void cudaFree(T *ptr)
+        void cudaFree(void *ptr)
         {
-            delete[] ptr;
             printCppError(!isAllocated(ptr),
                           "Wrong address for cudaFree",
                           __FILE__, __LINE__);
+            delete[] reinterpret_cast<char *>(ptr);
             allocated_addrs.erase(reinterpret_cast<intptr_t>(ptr));
         }
 
-        bool isAllocated(void *ptr)
+        // byte-level read
+        std::vector<u8> read(void *ptr, u64 size)
         {
-            return allocated_addrs.count(reinterpret_cast<intptr_t>(ptr));
+            auto ptr_addr = reinterpret_cast<intptr_t>(ptr);
+            for (auto &addr : allocated_addrs)
+            {
+                if (addr.first <= ptr_addr &&
+                    ptr_addr < addr.first + addr.second)
+                {
+                    printCppError(ptr_addr - addr.first + size > addr.second,
+                                  "Wrong size for read", __FILE__, __LINE__);
+                    std::vector<u8> ret(size);
+                    std::copy((char *)ptr, (char *)ptr + size, (char *)ret.data());
+                    return ret;
+                }
+            }
+            printCppError(true, "Wrong address for read", __FILE__, __LINE__);
+        }
+
+        // byte-level write
+        void write(void *ptr, const std::vector<u8> &data)
+        {
+            auto ptr_addr = reinterpret_cast<intptr_t>(ptr);
+            for (auto &addr : allocated_addrs)
+            {
+                if (addr.first <= ptr_addr &&
+                    ptr_addr < addr.first + addr.second)
+                {
+                    printCppError(ptr_addr - addr.first + data.size() > addr.second,
+                                  "Wrong size for write", __FILE__, __LINE__);
+                    std::copy((char *)data.data(), (char *)data.data() + data.size(), (char *)ptr);
+                    return;
+                }
+            }
+            printCppError(true, "Wrong address for write", __FILE__, __LINE__);
         }
     };
 
