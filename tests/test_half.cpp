c62d486a1f664fb2fe0022a3b4a3e6339bbd22e6
CWHer <cwher@outlook.com>
2022-12-26 17:33:07 +0000

    fix: update half

--- tests/test_half.cpp
+++ tests/test_half.cpp
@@ -3,56 +3,55 @@
 
 TEST(half, conversion)
 {
-    Sim::Half::FP32 x;
-    Sim::Half h;
+    Sim::f32 f;
+    Sim::f16 h;
 
     const float eps = 1e-4f;
 
     // normalized value
-    x.f = 10.0f;
-    h.fromFloat(x.f);
-    EXPECT_NEAR(x.f, h.toFloat(), x.f * eps);
+    f = 10.0f;
+    h = Sim::__float2half(f);
+    EXPECT_NEAR(f, Sim::__half2float(h), f * eps);
 
     // normalized value: overflow
-    x.f = std::numeric_limits<float>::max();
-    h.fromFloat(x.f);
-    EXPECT_TRUE(std::isinf(h.toFloat()));
+    f = std::numeric_limits<float>::max();
+    h = Sim::__float2half(f);
+    EXPECT_TRUE(std::isinf(Sim::__half2float(h)));
 
     // normalized value: underflow
-    x.f = std::numeric_limits<float>::min();
-    h.fromFloat(x.f);
-    EXPECT_EQ(0, h.toFloat());
+    f = std::numeric_limits<float>::min();
+    h = Sim::__float2half(f);
+    EXPECT_EQ(0, Sim::__half2float(h));
 
     // normalized value: underflow
-    x.f = 1.0f / (1 << 20);
-    h.fromFloat(x.f);
-    EXPECT_NEAR(x.f, h.toFloat(), x.f * eps);
+    f = 1.0f / (1 << 20);
+    h = Sim::__float2half(f);
+    EXPECT_NEAR(f, Sim::__half2float(h), f * eps);
 
     // denormalized value
-    x.f = std::numeric_limits<float>::denorm_min();
-    h.fromFloat(x.f);
-    EXPECT_EQ(0, h.toFloat());
+    f = std::numeric_limits<float>::denorm_min();
+    h = Sim::__float2half(f);
+    EXPECT_EQ(0, Sim::__half2float(h));
 
     // infinity
-    x.f = std::numeric_limits<float>::infinity();
-    h.fromFloat(x.f);
-    EXPECT_TRUE(std::isinf(h.toFloat()));
+    f = std::numeric_limits<float>::infinity();
+    h = Sim::__float2half(f);
+    EXPECT_TRUE(std::isinf(Sim::__half2float(h)));
 
     // NaN
-    x.f = std::numeric_limits<float>::quiet_NaN();
-    h.fromFloat(x.f);
-    EXPECT_TRUE(std::isnan(h.toFloat()));
+    f = std::numeric_limits<float>::quiet_NaN();
+    h = Sim::__float2half(f);
+    EXPECT_TRUE(std::isnan(Sim::__half2float(h)));
 }
 
 TEST(half, operators)
 {
-    Sim::Half h1, h2;
+    Sim::f16 h1, h2;
 
     const float eps = 1e-10f;
 
-    h1.fromFloat(1.0f);
-    h2.fromFloat(20000.0f);
+    h1 = Sim::__float2half(1.0f);
+    h2 = Sim::__float2half(20000.0f);
 
-    EXPECT_NEAR(20001.0f, h1 + h2, eps);
     EXPECT_NEAR(20000.0f, h1 * h2, eps);
 }
