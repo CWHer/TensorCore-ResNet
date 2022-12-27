34a0ffb61e26102eb8ebdfb4eeafe6546d418dc4
CWHer <cwher@outlook.com>
2022-12-27 17:48:04 +0000

    test: add irregular gemm test

--- tests/test_functions.cpp
+++ tests/test_functions.cpp
@@ -33,31 +33,31 @@ TEST(functions, gemm)
         for (int j = 0; j < m; j++)
             b[i + j * m] = i + j * m;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < k; j++)
-            std::cout << a[i * k + j] << ' ';
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-    for (int i = 0; i < k; i++)
-    {
-        for (int j = 0; j < m; j++)
-            std::cout << b[i + j * m] << ' ';
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < k; j++)
+    //         std::cout << a[i * k + j] << ' ';
+    //     std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
+    // for (int i = 0; i < k; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //         std::cout << b[i + j * m] << ' ';
+    //     std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
 
     Sim::host_gemm(a, b, c, n, m, k, sim);
     naiveMatmul(a, b, std_c, n, m, k);
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < k; j++)
-            std::cout << std_c[i * k + j] << ' ';
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //         std::cout << c[i * m + j] << ' ';
+    //     std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
 
     const float eps = 1e-5;
     for (int i = 0; i < n; i++)
@@ -65,41 +65,53 @@ TEST(functions, gemm)
             EXPECT_NEAR(c[i * m + j], std_c[i * m + j], eps);
 }
 
-// TEST(functions, gemm_irregular)
-// {
-//     Sim::GlobalMemory mem;
-//     Sim::RegisterFile reg_file;
-//     Sim::PRegisterFile preg_file;
-//     Sim::SRegisterFile sreg_file;
+TEST(functions, gemm_irregular)
+{
+    Sim::GlobalMemory mem;
+    Sim::RegisterFile reg_file;
+    Sim::PRegisterFile preg_file;
+    Sim::SRegisterFile sreg_file;
 
-//     Sim::GPUSimulator sim(mem, reg_file, preg_file, sreg_file);
+    Sim::GPUSimulator sim(mem, reg_file, preg_file, sreg_file);
 
-//     // int n = 171, m = 131, k = 91;
-//     int n = 16, m = 16, k = 16;
-//     Sim::f32 a[n * k], b[k * m], c[n * m], std_c[n * m];
+    int n = 171, m = 131, k = 91;
+    Sim::f32 a[n * k], b[k * m], c[n * m], std_c[n * m];
 
-//     for (int i = 0; i < 16; i++)
-//         for (int j = 0; j < 16; j++)
-//         {
-//             a[i * 16 + j] = i * 16 + j;
-//             b[i + j * 16] = i + j * 16;
-//         }
-//     for (int i = 0; i < 16; i++)
-//         for (int j = 0; j < 16; j++)
-//             std::cout << a[i * 16 + j]
-//                       << (j == 15 ? '\n' : ' ');
-//     std::cout << std::endl;
-//     for (int i = 0; i < 16; i++)
-//         for (int j = 0; j < 16; j++)
-//             std::cout << b[i + j * 16]
-//                       << (j == 15 ? '\n' : ' ');
-//     std::cout << std::endl;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < k; j++)
+            a[i * k + j] = std::sin(i * 2.0 + j * 3.0);
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < m; j++)
+            b[i + j * m] = std::cos(i * 3.0 + j * 2.0);
 
-//     Sim::host_gemm(a, b, c, n, m, k, sim);
-//     naiveMatmul(a, b, std_c, n, m, k);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < k; j++)
+    //         std::cout << a[i * k + j] << ' ';
+    //     std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
+    // for (int i = 0; i < k; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //         std::cout << b[i + j * m] << ' ';
+    //     std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
 
-//     const float eps = 1e-5;
-//     for (int i = 0; i < n; i++)
-//         for (int j = 0; j < m; j++)
-//             EXPECT_NEAR(c[i * m + j], std_c[i * m + j], eps);
-// }
+    Sim::host_gemm(a, b, c, n, m, k, sim);
+    naiveMatmul(a, b, std_c, n, m, k);
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //         std::cout << std_c[i * m + j] << ' ';
+    //     std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
+
+    const float eps = 1e-5;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            EXPECT_NEAR(c[i * m + j], std_c[i * m + j], eps);
+}
