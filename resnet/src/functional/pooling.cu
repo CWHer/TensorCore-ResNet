23426c00e73650cd6df9202b10186a0e5f418908
CWHer <cwher@outlook.com>
2022-12-29 18:57:48 +0000

    fix: update max pooling

--- resnet/src/functional/pooling.cu
+++ resnet/src/functional/pooling.cu
@@ -1,43 +1,40 @@
 #include <cfloat>
 #include "common.h"
 
-// TODO: tiling and leverage cache
 template <int block_size>
-__global__ void deviceMaxPool2dKernel(const float *input_data, int height, int width,
+__global__ void deviceMaxPool2dKernel(float *input_data, int height, int width,
                                       float *output_data, int out_height, int out_width,
                                       int kernel_size, int padding, int stride)
 {
-    // NOTE: B/2 (block) x 128 (thread) x H x W (within)
-    int input_grid_offset = blockDim.x * height * width;
-    int output_grid_offset = blockDim.x * out_height * out_width;
-    int input_thread_offset = height * width;
-    int output_thread_offset = out_height * out_width;
-
-    input_data += blockIdx.x * input_grid_offset +
-                  threadIdx.x * input_thread_offset;
-    output_data += blockIdx.x * output_grid_offset +
-                   threadIdx.x * output_thread_offset;
-
     // HACK: NOTE: Max-pooling uses implicit negative infinity padding,
     //  not zero-padding as indicated in documentation
     // https://github.com/pytorch/pytorch/issues/33384
+    int input_grid_offset = blockIdx.x * height * width;
+    int output_grid_offset = blockIdx.x * out_height * out_width;
 
-    int output_offset = 0;
-    for (int i = kernel_size - 1 - padding; i < height + padding; i += stride)
-        for (int j = kernel_size - 1 - padding; j < width + padding; j += stride)
-        {
-            float ret = -FLT_MAX;
-            for (int x = i - kernel_size + 1; x <= i; x++)
-                for (int y = j - kernel_size + 1; y <= j; y++)
-                {
-                    float value = 0 <= x && x < height &&
-                                          0 <= y && y < width
-                                      ? input_data[x * width + y]
-                                      : -FLT_MAX; // negative infinity padding
-                    ret = max(ret, value);
-                }
-            output_data[output_offset++] = ret;
-        }
+    int row = block_size * threadIdx.y;
+    int col = block_size * threadIdx.x;
+    for (int i = 0; i < block_size; i++)
+        for (int j = 0; j < block_size; j++)
+            if (row + i < out_height && col + j < out_width)
+            {
+                float value = -FLT_MAX;
+                for (int x = 0; x < kernel_size; ++x)
+                    for (int y = 0; y < kernel_size; ++y)
+                    {
+                        int input_row = (row + i) * stride + x - padding;
+                        int input_col = (col + j) * stride + y - padding;
+                        if (0 <= input_row && input_row < height &&
+                            0 <= input_col && input_col < width)
+                        {
+                            float input_value = input_data[input_grid_offset +
+                                                           input_row * width + input_col];
+                            value = max(value, input_value);
+                        }
+                    }
+                output_data[output_grid_offset +
+                            (row + i) * out_width + (col + j)] = value;
+            }
 }
 
 // TODO: tiling and leverage cache
@@ -86,15 +83,17 @@ void maxpool2d(const float *input_data,
                int out_width,
                int kernel_size,
                int padding,
-               int stride) {
-  static const int N_THREADS = 128;
-  dim3 block_dim(N_THREADS);
-  dim3 grid_dim(batch_size * num_channels / N_THREADS);
-  deviceMaxPool2dKernel<32><<<grid_dim, block_dim>>>(
-      input_data, height, width, output_data, out_height, out_width,
-      kernel_size, padding, stride);
+               int stride)
+{
+    static const int N_THREADS = 16;
+    dim3 grid_dim(batch_size * num_channels);
+    dim3 block_dim(N_THREADS, N_THREADS);
+    checkCppErrorsMsg(height != width || out_height != out_width,
+                      "Only square images are supported");
+    deviceMaxPool2dKernel<4><<<grid_dim, block_dim>>>(
+        (float *)input_data, height, width, output_data, out_height, out_width,
+        kernel_size, padding, stride);
 }
-
 
 void avgpool2d(const float *input_data,
                int batch_size,
