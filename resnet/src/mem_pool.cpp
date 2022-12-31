521d916c3adaf7654fb996a420fcc741ad9bfd49
Weihao Jiang <weihau.chiang@gmail.com>
2022-12-31 19:29:44 +0800

    perf(stepped memory pool): reduce pool usage by grouping near-sized memories.

--- resnet/src/mem_pool.cpp
+++ resnet/src/mem_pool.cpp
@@ -9,31 +9,49 @@ using namespace Impl;
 using namespace std;
 
 static std::unordered_map<uint64_t, std::deque<void *>> mem_cache;
-static std::unordered_map<void*, uint64_t> mem_size;
+static std::unordered_map<void *, uint64_t> mem_size;
+
+static constexpr size_t cache_memory_step = 512 * 1024;
 
 namespace Impl {
 
 void init_mem_pool() {}
 
 void deinit_mem_pool() {
+#if DEBUG
+  vector<pair<uint64_t, int>> sizes;
+  for (auto &kv : mem_cache) {
+    sizes.push_back({kv.first, kv.second.size()});
+  }
+  sort(sizes.begin(), sizes.end(), [](const pair<uint64_t, int> &a, const pair<uint64_t, int> &b) {
+    return a.first < b.first;
+  });
+  auto total = 0;
+  for (auto &kv : sizes) {
+    cout << "mem_pool: " << kv.first / cache_memory_step << "*512KB" << ": " << kv.second << endl;
+    total += kv.first * kv.second;
+  }
+
+  total /= 1024 * 1024;
+
+  cout << "mem_pool: total " << total << "MB" << endl;
+#endif
+
   for (auto &kv : mem_cache)
     for (auto &ptr : kv.second)
       checkCudaErrors(cudaFree(ptr));
 }
 
 cudaError_t cudaPooledMalloc(void **devPtr, size_t size) {
-  if (mem_cache.count(size) > 0 && !mem_cache[size].empty())
-  {
-    *devPtr = mem_cache[size].front();
-    mem_cache[size].pop_front();
+  auto stepped_size = (size + cache_memory_step - 1) / cache_memory_step * cache_memory_step;
+  if (mem_cache.count(stepped_size) > 0 && !mem_cache[stepped_size].empty()) {
+    *devPtr = mem_cache[stepped_size].front();
+    mem_cache[stepped_size].pop_front();
     return cudaSuccess;
-  }
-  else
-  {
-    auto ret = cudaMalloc(devPtr, size);
-    if (ret == cudaSuccess)
-    {
-      mem_size[*devPtr] = size;
+  } else {
+    auto ret = cudaMalloc(devPtr, stepped_size);
+    if (ret == cudaSuccess) {
+      mem_size[*devPtr] = stepped_size;
     }
     return ret;
   }
