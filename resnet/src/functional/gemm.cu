e44a884c813af966fce22a3ba84274a6a90f6184
Weihao Jiang <weihau.chiang@gmail.com>
2023-01-02 03:54:00 +0800

    feat(simulator boosting): use std::thread to simulate cuda stream.

--- resnet/src/functional/gemm.cu
+++ resnet/src/functional/gemm.cu
@@ -295,3 +295,17 @@ void gemm_batched_B(const float_16 *A,
   }
 }
 
+
+void gemm(const float_16 *A,
+          const float_16 *B,
+          float_32 *C,
+          size_t M,
+          size_t N,
+          size_t K,
+          const GEMM::Major major,
+          const Impl::DeviceType device_type) {
+  cudaStream_t stream = nullptr;
+  gemm_stream(A, B, C, M, N, K, major, device_type, stream);
+  cudaStreamSynchronize(stream);
+  cudaCacheCommit(stream);
+}
--- resnet/src/simulator/gemm.cpp
+++ resnet/src/simulator/gemm.cpp
@@ -10,9 +10,14 @@
 
 using namespace Impl;
 
+// HACK: map cudaStream to a std::thread to simulate multi async stream behavior
+static std::unordered_map<cudaStream_t, std::thread> sim_stream_threads;
+// Guard mem_pool related operations as they are not thread safe
+static std::mutex mem_pool_ops_mutex;
+
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "misc-no-recursion"
-void gemm_stream(const float_16 *A,
+static void gemm_stream_threaded(const float_16 *A,
                  const float_16 *B,
                  float_32 *C,
                  size_t M,
@@ -20,9 +25,9 @@ void gemm_stream(const float_16 *A,
                  size_t K,
                  const GEMM::Major major,
                  const Impl::DeviceType device_type,
-                 cudaStream_t &stream [[gnu::unused]]) {
+                 cudaStream_t &stream) {
   if (major == GEMM::Major::col_major) {
-    return gemm_stream(B, A, C, N, M, K, GEMM::Major::row_major, device_type, stream);
+    return gemm_stream_threaded(B, A, C, N, M, K, GEMM::Major::row_major, device_type, stream);
   }
 
   Sim::GlobalMemory mem;
@@ -37,8 +42,10 @@ void gemm_stream(const float_16 *A,
   if (device_type == Impl::DeviceType::CPU) {
     A_host = fp16_array_to_fp32_array(A, M * K, device_type);
     B_host = fp16_array_to_fp32_array(B, K * N, device_type);
-    C_host = C;
+    C_host = new float[M * N];
   } else {
+    const std::lock_guard<std::mutex> lock(mem_pool_ops_mutex);
+
     float *A_dev, *B_dev;
     A_dev = fp16_array_to_fp32_array(A, M * K, device_type);
     B_dev = fp16_array_to_fp32_array(B, K * N, device_type);
@@ -60,9 +67,14 @@ void gemm_stream(const float_16 *A,
 #pragma clang diagnostic pop
 
   if (device_type == Impl::DeviceType::CPU) {
+    // Guard copy
+    const std::lock_guard<std::mutex> lock(mem_pool_ops_mutex);
+    std::copy(C_host, C_host + M * N, C);
     delete[] A_host;
     delete[] B_host;
+    delete[] C_host;
   } else {
+    const std::lock_guard<std::mutex> lock(mem_pool_ops_mutex);
     cudaMemcpy(C, C_host, M * N * sizeof(float), cudaMemcpyHostToDevice);
     delete[] A_host;
     delete[] B_host;
@@ -71,6 +83,23 @@ void gemm_stream(const float_16 *A,
 
 }
 #pragma clang diagnostic pop
+
+void gemm_stream(const float_16 *A,
+                          const float_16 *B,
+                          float_32 *C,
+                          size_t M,
+                          size_t N,
+                          size_t K,
+                          const GEMM::Major major,
+                          const Impl::DeviceType device_type,
+                          cudaStream_t &stream) {
+  if (sim_stream_threads.find(stream) == sim_stream_threads.end()) {
+    sim_stream_threads[stream] = std::thread(gemm_stream_threaded, A, B, C, M, N, K, major, device_type, std::ref(stream));
+  } else {
+    sim_stream_threads[stream].join();
+    sim_stream_threads[stream] = std::thread(gemm_stream_threaded, A, B, C, M, N, K, major, device_type, std::ref(stream));
+  }
+}
 
 void gemm_batched_B(const float_16 *A,
                     const float_16 *B,
@@ -101,10 +130,27 @@ void gemm_batched_B(const float_16 *A,
   }
 
   for (auto &stream : streams) {
+    sim_stream_threads[stream].join();
+    sim_stream_threads.erase(stream);
     cudaStreamSynchronize(stream);
     cudaStreamDestroy(stream);
     cudaCacheCommit(stream);
   }
 }
 
+void gemm(const float_16 *A,
+          const float_16 *B,
+          float_32 *C,
+          size_t M,
+          size_t N,
+          size_t K,
+          const GEMM::Major major,
+          const Impl::DeviceType device_type) {
+  cudaStream_t stream = nullptr;
+  gemm_stream(A, B, C, M, N, K, major, device_type, stream);
+  sim_stream_threads[stream].join();
+  sim_stream_threads.erase(stream);
+  cudaStreamSynchronize(stream);
+  cudaCacheCommit(stream);
+}
 
